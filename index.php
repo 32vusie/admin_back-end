e11342274e6e4358fd375e72285b81fccba9c584
32vusie <77724400+32vusie@users.noreply.github.com>
2021-01-20 11:39:52 +0100

    index setup

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once './php/seps/header.php'; ?>
+<body>
+
+<?php include_once './php/seps/nav.php'; ?>
+
+<div class="container-fluid">
+    <div class="row content">
+        <?php include_once './php/seps/sidenav.php'; ?>
+    <br>
+    <?php include_once './php/seps/cards.php' ?>
+  </div>
+</div>
+
+</body>
+</html>
---
+++ php/seps/nav.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-inverse visible-xs">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>                        
+            </button>
+            <a class="navbar-brand" href="#">System32Online</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Dashboard</a></li>
+            <li><a href="#">Website</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">Navigation</a></li>
+            <li><a href="#">Files</a></li>
+            <li><a href="#">settings</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
